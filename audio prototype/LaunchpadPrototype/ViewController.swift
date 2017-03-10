5ce7ccec247cb32a7e1777bf3d5ac34ff56e2b45
Ng Zhi An <ngzhian@gmail.com>
2017-03-10 14:19:16 +0800

    After some hacking, finally got insets and ui collectionview to look alright

--- audio prototype/LaunchpadPrototype/ViewController.swift
+++ audio prototype/LaunchpadPrototype/ViewController.swift
@@ -14,11 +14,12 @@ class ViewController: UIViewController,
     UIGestureRecognizerDelegate {
 
     @IBOutlet weak var buttonCollectionView: UICollectionViewExtension!
+    var auxButtonCollectionView: UICollectionView!
 
     var DEFAULT_BUTTON_SIZE = CGSize(width: 0, height: 0)
     let ITEMS_PER_ROW = 8
     let NUMBER_OF_SECTIONS = 6
-    let SECTION_INSETS = UIEdgeInsets(top: 10, left: 10, bottom: 0, right: 10)
+    let SECTION_INSETS = UIEdgeInsets(top: 0, left: 0, bottom: 10, right: 0)
     let ITEM_INSETS = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
 
     let reuseIdentifier = "cell"
@@ -26,13 +27,34 @@ class ViewController: UIViewController,
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // fix the width of the button collection view
+        let totalWidth = self.view.frame.width - 20 - 20 // padding left and right
+        // left with 9 columns of buttons with 8 insets in between
+        // so to get width for the pads we add 1 inset and times 8/9
+        let padWidth = (totalWidth + ITEM_INSETS.right) * 8 / 9
+        let padHeight = padWidth / 8 * 6
+        let auxWidth = (totalWidth + ITEM_INSETS.right) / 9
+        buttonCollectionView.frame = CGRect(
+            origin: buttonCollectionView.frame.origin,
+            size: CGSize(width: padWidth, height: padHeight))
+
+        auxButtonCollectionView = UICollectionView(
+            frame: CGRect(
+                origin: CGPoint(x: padWidth + ITEM_INSETS.right, y: buttonCollectionView.frame.minY),
+                size: CGSize(width: auxWidth, height: padHeight)),
+            collectionViewLayout: UICollectionViewLayout()
+        )
+        auxButtonCollectionView.backgroundColor = UIColor.blue;
+
         self.view.backgroundColor = Constants.BackgroundColor
         // set the collection view background to transparent
         buttonCollectionView.backgroundColor = nil
         buttonCollectionView.isUserInteractionEnabled = true
         buttonCollectionView.isMultipleTouchEnabled = true
-        calculateWidthPerItem()
         buttonCollectionView.startListenAudio()
+
+        self.view.addSubview(auxButtonCollectionView)
     }
    
     override func didReceiveMemoryWarning() {
@@ -40,17 +62,6 @@ class ViewController: UIViewController,
         // Dispose of any resources that can be recreated.
     }
 
-    private func calculateWidthPerItem() {
-        let totalLength = buttonCollectionView.frame.width
-        var insetLength = ITEM_INSETS.left * CGFloat(ITEMS_PER_ROW - 1)
-        insetLength += SECTION_INSETS.left + SECTION_INSETS.right
-        let availableLength = totalLength - insetLength
-        let itemLength = availableLength / CGFloat(ITEMS_PER_ROW)
-        DEFAULT_BUTTON_SIZE = CGSize(width: itemLength, height: itemLength)
-    }
-
-    //collecton view functions
-    // tell the collection view how many cells to make
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return ITEMS_PER_ROW
     }
@@ -62,6 +73,13 @@ class ViewController: UIViewController,
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let totalLength = buttonCollectionView.frame.width
+        let insetLength = ITEM_INSETS.left * (CGFloat(ITEMS_PER_ROW + 1))
+        let availableLength = totalLength - insetLength
+        let itemLength = availableLength / CGFloat(ITEMS_PER_ROW)
+
+        DEFAULT_BUTTON_SIZE = CGSize(width: itemLength, height: itemLength)
+        print(DEFAULT_BUTTON_SIZE)
         return DEFAULT_BUTTON_SIZE
     }
 
