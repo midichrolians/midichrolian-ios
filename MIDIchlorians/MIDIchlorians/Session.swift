541daf701d159109726a08842d216397dc424d1a
Advay Pal <advaypal@hotmail.com>
2017-03-18 18:56:08 +0800

    Finish session class

--- MIDIchlorians/MIDIchlorians/Session.swift
+++ MIDIchlorians/MIDIchlorians/Session.swift
@@ -7,15 +7,30 @@
 //
 import RealmSwift
 
+/**
+ This class represents a Session, which is a collection of grids, each grid being a collection
+ of pads. A session also has a BPM associated with it.
+ Though a default initialiser is valid(because of Realm), it will lead to incorrect results. 
+ For correct results, use the convenience initialiser defined below.
+ **/
 class Session: Object {
     
     dynamic private var BPM = 0 // Need to agree on a default value
-    dynamic private var numPages = Config.numberOfPages
-    dynamic private var numRows = Config.numberOfRows
-    dynamic private var numCols = Config.numberOfColumns
+    dynamic private var numPages = 0
+    dynamic private var numRows = 0
+    dynamic private var numCols = 0
     private let padList = List<Pad>()
     
-    private var pads: [[[Pad]]] = []
+    private var pads = [[[Pad]]]()
+    
+    convenience init(bpm: Int, numPages: Int, numRows: Int, numCols: Int) {
+        self.init()
+        self.BPM = bpm
+        self.numPages = numPages
+        self.numRows = numRows
+        self.numCols = numCols
+        initialisePadGrid()
+    }
     
     //Tells Realm that thse properties should not be persisted
     override static func ignoredProperties() -> [String] {
@@ -73,4 +88,28 @@ class Session: Object {
             col < numCols && col >= 0
     }
     
+    //Initalises the list, as that is what is saved. The pads matrix is not saved.
+    func prepareForSave() {
+        for page in 0..<numPages {
+            for row in 0..<numRows {
+                for col in 0..<numCols {
+                    padList.append(pads[page][row][col])
+                }
+            }
+        }
+    }
+    
+    //Loads from List into Matrix. Realm guarantees that order of insertion is maintained.
+    func load() {
+        for page in 0..<numPages {
+            pads.append([])
+            for row in 0..<numRows {
+                pads[page].append([])
+                for col in 0..<numCols {
+                    let listIndex = (page * numRows * numCols) + (row * numCols) + col
+                    pads[page][row].append(padList[listIndex])
+                }
+            }
+        }
+    }
 }
