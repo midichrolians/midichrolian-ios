a525f3f1088670d4378baecc71ab90ce10645730
Ng Zhi An <ngzhian@gmail.com>
2017-03-23 11:33:37 +0800

    Edit button (#13)
    * Add edit + add buttons for tables
    
    * Add skeleton to update models when sample/animation selected
    
    * Set animation when tapped

--- MIDIchlorians/MIDIchlorians/GridCollectionView.swift
+++ MIDIchlorians/MIDIchlorians/GridCollectionView.swift
@@ -16,10 +16,12 @@ class GridCollectionView: UICollectionView, UICollectionViewDataSource, UICollec
             }
         }
     }
+    internal var currentSession: Session?
+    internal var currentPage = 0
 
     private let reuseIdentifier = "cell"
     private let audioManager = AudioManager()
-    private var selectedPad: IndexPath?
+    internal var selectedPad: IndexPath?
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return Config.numberOfColumns
@@ -158,5 +160,30 @@ class GridCollectionView: UICollectionView, UICollectionViewDataSource, UICollec
                 return
             }
         }
+    }
+}
+
+extension GridCollectionView: SampleTableDelegate {
+    func sampleTable(_: UITableView, didSelect sample: String) {
+        guard let row = self.selectedPad?.section, let col = self.selectedPad?.row else {
+            return
+        }
+        self.currentSession?.addAudio(page: self.currentPage, row: row, col: col, audioFile: sample)
+    }
+}
+
+extension GridCollectionView: AnimationTableDelegate {
+    func animationTable(_: UITableView, didSelect animation: String) {
+        guard let indexPath = self.selectedPad else {
+            return
+        }
+
+        guard let animationSequence = AnimationTypes.getAnimationSequenceForAnimationType(
+            animationTypeName: animation, indexPath: indexPath) else {
+                return
+        }
+
+        self.currentSession?.addAnimation(
+            page: self.currentPage, row: indexPath.section, col: indexPath.row, animation: animationSequence)
     }
 }
