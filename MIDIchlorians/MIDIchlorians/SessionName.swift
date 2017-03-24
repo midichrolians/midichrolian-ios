0482c32b37282c04b39137e4bf884afd6caefe01
Advay Pal <advaypal@hotmail.com>
2017-03-25 02:02:10 +0800

    Add new model for session name:

---
+++ MIDIchlorians/MIDIchlorians/SessionName.swift
@@ -0,0 +1,19 @@
+//
+//  SessionName.swift
+//  MIDIchlorians
+//
+//  Created by Advay Pal on 25/03/17.
+//  Copyright © 2017 nus.cs3217.a0118897. All rights reserved.
+//
+
+import RealmSwift
+
+class SessionName: Object {
+
+    private var sessionName: String = ""
+
+    convenience init(_ sessionString: String) {
+        self.init()
+        self.sessionName = sessionString
+    }
+}
