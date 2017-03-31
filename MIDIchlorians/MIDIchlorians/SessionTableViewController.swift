21eed55492631bfc749d4932b27429d27b0487c4
Ng Zhi An <ngzhian@gmail.com>
2017-03-31 15:00:53 +0800

    Change hardcode session list to get from view controller

--- MIDIchlorians/MIDIchlorians/SessionTableViewController.swift
+++ MIDIchlorians/MIDIchlorians/SessionTableViewController.swift
@@ -11,16 +11,25 @@ import UIKit
 class SessionTableViewController: UITableViewController {
     weak var delegate: SessionTableDelegate?
 
-    private let data = ["Session 1", "Session 2"]
+    var sessions: [String] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     private let reuseIdentifier = Config.SessionTableReuseIdentifier
     private let newSessionButton = UIBarButtonItem(barButtonSystemItem: .add, target: nil, action: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.title = "Sessions"
+        self.title = Config.SessionTableTitle
         self.navigationItem.rightBarButtonItem = self.editButtonItem
         self.navigationItem.leftBarButtonItem = self.newSessionButton
+
+        // have to set target/action here for this to work, not above in the inistnatiation.
+        self.newSessionButton.target = self
+        self.newSessionButton.action = #selector(newSession(barButton:))
+
         self.tableView.register(SessionTableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
         self.tableView.separatorColor = Config.TableViewSeparatorColor
     }
@@ -37,7 +46,7 @@ class SessionTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        return sessions.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -46,15 +55,34 @@ class SessionTableViewController: UITableViewController {
                 return SessionTableViewCell()
         }
 
-        cell.textLabel?.text = data[indexPath.row]
+        cell.textLabel?.text = sessions[indexPath.row]
 
         return cell
     }
 
     override func tableView(_ tableView: UITableView,
+                            commit editingStyle: UITableViewCellEditingStyle,
+                            forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let deletedSession = sessions.remove(at: indexPath.row)
+            delegate?.sessionTable(tableView, didRemove: deletedSession)
+        }
+    }
+
+    // MARK: - Table view delegate
+
+    override func tableView(_ tableView: UITableView,
                             willDisplay cell: UITableViewCell,
                             forRowAt indexPath: IndexPath) {
         cell.textLabel?.textColor = Config.SessionTableViewCellColor
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        delegate?.sessionTable(tableView, didSelect: sessions[indexPath.row])
+    }
+
+    func newSession(barButton: UIBarButtonItem) {
+        delegate?.sessionTable(tableView)
     }
 
 }
