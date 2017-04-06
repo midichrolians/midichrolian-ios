5608e3199401af2fc8a836a09834cddf0802b321
Ng Zhi An <ngzhian@gmail.com>
2017-04-07 00:37:25 +0800

    Fix not adding child vc

--- MIDIchlorians/MIDIchlorians/ViewController.swift
+++ MIDIchlorians/MIDIchlorians/ViewController.swift
@@ -100,6 +100,7 @@ class ViewController: UIViewController {
     // Sets up the main grid for play/edit
     private func setUpGrid() {
         gridController = GridController(frame: CGRect.zero, session: currentSession)
+        addChildViewController(gridController)
 
         view.addSubview(gridController.view)
 
