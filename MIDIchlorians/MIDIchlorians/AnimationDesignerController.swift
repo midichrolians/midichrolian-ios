cbd86f449f97883164050737f17fb75dda6b81bc
Ng Zhi An <ngzhian@gmail.com>
2017-04-07 14:24:34 +0800

    Disallow save when name is empty

--- MIDIchlorians/MIDIchlorians/AnimationDesignerController.swift
+++ MIDIchlorians/MIDIchlorians/AnimationDesignerController.swift
@@ -17,6 +17,7 @@ class AnimationDesignerController: UIViewController {
 
     private var animationTypeSegmentedControl: UISegmentedControl!
     private var saveButton: UIButton!
+    internal var saveAlert: UIAlertController!
 
     private var colourLabel: UILabel!
     internal var colourPicker: ColourCollectionViewController!
@@ -66,6 +67,20 @@ class AnimationDesignerController: UIViewController {
         saveButton.setTitle(Config.AnimationDesignSaveLabel, for: .normal)
         saveButton.addTarget(self, action: #selector(saveButtonTapped), for: .touchDown)
         view.addSubview(saveButton)
+
+        saveAlert = UIAlertController(title: Config.AnimationSaveAlertTitle, message: nil, preferredStyle: .alert)
+        saveAlert.addAction(
+            UIAlertAction(title: Config.AnimationSaveOkayTitle, style: .default, handler: { _ in
+                guard let newName = self.saveAlert.textFields?.first?.text else {
+                    return
+                }
+                if newName.isEmpty {
+                    return
+                }
+                self.delegate?.saveAnimation(name: newName)
+            }))
+        saveAlert.addAction(UIAlertAction(title: Config.AnimationSaveCancelTitle, style: .cancel, handler: nil))
+        saveAlert.addTextField(configurationHandler: { $0.delegate = self })
 
         setConstraints()
     }
@@ -123,17 +138,7 @@ class AnimationDesignerController: UIViewController {
     }
 
     func saveButtonTapped() {
-        let alert = UIAlertController(title: Config.AnimationSaveAlertTitle, message: nil, preferredStyle: .alert)
-        alert.addAction(
-            UIAlertAction(title: Config.AnimationSaveOkayTitle, style: .default, handler: { _ in
-                guard let newName = alert.textFields?.first?.text else {
-                    return
-                }
-                self.delegate?.saveAnimation(name: newName)
-            }))
-        alert.addAction(UIAlertAction(title: Config.AnimationSaveCancelTitle, style: .cancel, handler: nil))
-        alert.addTextField()
-        self.present(alert, animated: true, completion: nil)
+        self.present(saveAlert, animated: true, completion: nil)
     }
 }
 
@@ -173,5 +178,29 @@ extension AnimationDesignerController: ColourPickerDelegate {
         // and also clear selected colour in palette to visually indicate
         selectedColour = nil
         colourSelection.position(at: indexPath)
+    }
+}
+
+extension AnimationDesignerController: UITextFieldDelegate {
+    // Don't allow return if the field is empty, user must explicitly cancel
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return !(textField.text?.isEmpty ?? true)
+    }
+
+    // Deactivate the Save button if text field is empty
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        guard let text = textField.text else {
+            return true
+        }
+
+        let str = (text as NSString).replacingCharacters(in: range, with: string)
+        if str.isEmpty {
+            saveAlert.actions.first?.isEnabled = false
+        } else {
+            saveAlert.actions.first?.isEnabled = true
+        }
+        return true
     }
 }
