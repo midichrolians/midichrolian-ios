78dc5dd4812f155053fcaa0e8a9586cb3fa377c4
Ng Zhi An <ngzhian@gmail.com>
2017-04-07 16:03:04 +0800

    Present upload/download option

---
+++ MIDIchlorians/MIDIchlorians/SyncViewController.swift
@@ -0,0 +1,73 @@
+//
+//  SyncViewController.swift
+//  MIDIchlorians
+//
+//  Created by Zhi An Ng on 7/4/17.
+//  Copyright © 2017 nus.cs3217.a0118897. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import SwiftyDropbox
+
+class SyncViewController: UIViewController {
+    private var upload: UIButton!
+    private var download: UIButton!
+    private var stackView: UIStackView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        upload = UIButton(type: .system)
+        upload.setTitle(Config.TopNavSyncUploadTitle, for: .normal)
+        upload.addTarget(self, action: #selector(onUpload), for: .touchDown)
+
+        download = UIButton(type: .system)
+        download.setTitle(Config.TopNavSyncDownloadTitle, for: .normal)
+        download.addTarget(self, action: #selector(onDownload), for: .touchDown)
+
+        stackView = UIStackView(arrangedSubviews: [upload, download])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+
+        view.addSubview(stackView)
+        self.preferredContentSize = CGSize(width: Config.TopNavSyncPreferredWidth,
+                                           height: Config.TopNavSyncPreferredHeight)
+
+        setConstraints()
+    }
+
+    func setConstraints() {
+        stackView.snp.makeConstraints { make in
+            make.edges.equalTo(view)
+        }
+    }
+
+    func onUpload() {
+        if DropboxClientsManager.authorizedClient == nil {
+            DropboxClientsManager.authorizeFromController(UIApplication.shared,
+                                                          controller: self,
+                                                          openURL: { (url: URL) -> Void in
+                                                            UIApplication.shared.open(url) },
+                                                          browserAuth: false)
+        } else {
+            CloudManager.instance.saveToDropbox()
+        }
+        dismiss(animated: true, completion: nil)
+    }
+
+    func onDownload() {
+        if DropboxClientsManager.authorizedClient == nil {
+            DropboxClientsManager.authorizeFromController(UIApplication.shared,
+                                                          controller: self,
+                                                          openURL: { (url: URL) -> Void in
+                                                            UIApplication.shared.open(url) },
+                                                          browserAuth: false)
+        } else {
+            CloudManager.instance.loadFromDropbox()
+        }
+        dismiss(animated: true, completion: nil)
+    }
+
+}
