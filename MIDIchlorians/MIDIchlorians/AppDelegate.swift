d9adf6edc8355a15fd18eba60151e37dffe11beb
Advay Pal <advaypal@hotmail.com>
2017-04-10 20:06:02 +0800

    Add fabric

--- MIDIchlorians/MIDIchlorians/AppDelegate.swift
+++ MIDIchlorians/MIDIchlorians/AppDelegate.swift
@@ -9,6 +9,8 @@
 import UIKit
 import RealmSwift
 import SwiftyDropbox
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -130,6 +132,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             // if saving fails, what are we gonna do?
             _ = DataManager.instance.saveAnimation(preloadedAnimation)
         }
+
+        Fabric.with([Crashlytics.self])
         
         return true
     }
